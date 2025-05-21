5065278edc2265f3dab4e0e9e158a767462bb696
Bryan Picon <bpicon@frba.utn.edu.ar>
2025-05-21 20:08:01 -0300

    Actualiza el mensaje de saludo en main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+using namespace std; 
+  
+// Realizar un programa que muestra por pantalla la leyenda “Hola Nombre Apellido” y dejar el cursor en la siguiente línea. Por supuesto que Nombre y Apellido debe reemplazarlos por los suyos. 
 
+int main ()
+{
+cout << "Hola Bryan Picon" << endl; 
+  return 0; 
+}
